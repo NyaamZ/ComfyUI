8e953a9158b0805fa5130729c8e702a3f1d4916d
NyaamZ <43065065+NyaamZ@users.noreply.github.com>
2025-01-21 02:13:00 +0900

    to use useToast
    to use useToast

--- web/scripts/ui/utils.js
+++ web/scripts/ui/utils.js
@@ -1,3 +1,4 @@
 // Shim for scripts/ui/utils.ts
 export const applyClasses = window.comfyAPI.utils.applyClasses;
 export const toggleElement = window.comfyAPI.utils.toggleElement;
+export const useToastStore = window.comfyAPI.utils.useToastStore;
